25827b28708492a0b69d4aa1b6c0d39991856797
Sang-GitHup020120 <sangolariel14@gmail.com>
2019-08-22 20:31:49 +0700

    complete schange slide

--- Sang/Sang-index.js
+++ Sang/Sang-index.js
@@ -15,12 +15,25 @@ function clickLeft(){
         sli.style.backgroundColor='yellow';
     }
     else
-
+        sli.style.backgroundColor='black';
 }
 function clickRight(){
+    let color=sli.style.backgroundColor; //lay duoc mau background
+    if(color==='black'){
+        sli.style.backgroundColor='green';
+    }
+    else if(color==='green'){
+        sli.style.backgroundColor='red';
+    }
+    else if(color==='red'){
+        sli.style.backgroundColor='yellow';
+    }
+    else
+        sli.style.backgroundColor='black';
 
 
 }
+sli.style.transition = '2s';
 
 aLeft.addEventListener('click', clickLeft);//dung EventListener bat su kien click NOTE::
 aRight.addEventListener('click', clickRight);//dung EventListener bat su kien click
